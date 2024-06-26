28b86bee9cc45dcbfa779006f5220f27edd0c918
Fred Lee <juhee.fred.lee@gmail.com>
2024-06-26 17:42:22 +0900

    [feat] 스프링 설정 추가

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -2,19 +2,21 @@ package hello.core;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
-import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
+import hello.core.member.MemberService;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
-import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        OrderService orderService = ac.getBean("orderService", OrderService.class);
         Member member = new Member(1L, "tester01", Grade.VIP);
-        appConfig.memberService().join(member);
+        memberService.join(member);
 
-        Order orderItem = appConfig.orderService().createOrder(member.getId(), "item1", 5000);
+        Order orderItem = orderService.createOrder(member.getId(), "item1", 5000);
         System.out.println("orderItem = " + orderItem);
     }
 }
